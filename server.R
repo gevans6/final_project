9f8a307dd0333e0e96aa716516ee1994ec786f39
Jessica Libman <jessicalibman@Jessicas-MacBook-Pro-2.local>
2016-03-10 22:46:55 -0800

    server and ui work

--- server.R
+++ server.R
@@ -4,9 +4,8 @@ library(dplyr)
 
 source('graph_SingleByAge .R')
 
-
 shinyServer(function(input, output) {
   output$scatter <- renderPlotly({ 
-    graph_SingleByAge(graph_SingleByAge(input$dfSelection, input$ySelection))
+    graph_SingleByAge(input$dfSelection, input$ySelection)
   })
 })
