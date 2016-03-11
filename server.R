c6a3d5d79552904c665d1bbe295776a432b61f6f
Jessica Libman <jessicalibman@Jessicas-MacBook-Pro-2.local>
2016-03-10 22:35:30 -0800

    ui and server radio buttons

--- server.R
+++ server.R
@@ -2,10 +2,11 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
+source('graph_SingleByAge .R')
 
 
 shinyServer(function(input, output) {
   output$scatter <- renderPlotly({ 
-      
+    graph_SingleByAge(graph_SingleByAge(input$dfSelection, input$ySelection))
   })
 })
