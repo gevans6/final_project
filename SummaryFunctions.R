f8645601aacbb41c229b2e2f8b66bdcbdf291083
Justin Koga <j.koga.8@gmail.com>
2016-03-10 23:12:29 -0800

    small changes

--- SummaryFunctions.R
+++ SummaryFunctions.R
@@ -5,5 +5,7 @@ SummaryInfo <- function(data) {
   summary <- head(data, n = 1L)
   summary$male_total <- df$Population[2]
   summary$female_total <- df$Population[3]
+  summary$male_percent <- df$PercentTotalEnrolled[2]
+  summary$female_percent <- df$PercentTotalEnrolled[3]
   return(summary)
 }
